86526b8b03025ff71d6944283bebe8d1ae62a6c4
GreyGreyman <greygreyman@gmail.com>
2024-07-29 11:25:52 +0300

    add docker configs for app and nginx, add elk with filebeat infra, add filebeat nginx logging

--- app/src/main.py
+++ app/src/main.py
@@ -41,7 +41,6 @@ app.include_router(reviews.router, prefix="/ugc/v1/reviews", tags=["reviews"])
 app.include_router(likes.router, prefix="/ugc/v1/likes", tags=["likes"])
 app.include_router(bookmarks.router, prefix="/ugc/v1/bookmarks", tags=["bookmark"])
 
-
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",

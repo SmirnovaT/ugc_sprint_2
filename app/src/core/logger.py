740ab6b2a7590120e3c73f88bc44349ea903ad2c
GreyGreyman <greygreyman@gmail.com>
2024-08-01 02:14:52 +0300

    add structured logging for app, config for filebeat and elk

--- app/src/core/logger.py
+++ app/src/core/logger.py
@@ -1,11 +1,112 @@
 import logging
-import sys
-from logging import Formatter, StreamHandler
+import logging.config
+from os import makedirs
 
-ugc_handler = StreamHandler(stream=sys.stdout)
-ugc_handler.setFormatter(
-    Formatter(fmt="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-)
+import structlog
 
-ugc_logger = logging.getLogger("ugc_service")
-ugc_logger.addHandler(ugc_handler)
+
+def configure_logger(enable_json_logs: bool = False):
+    timestamper = structlog.processors.TimeStamper(fmt="%Y-%m-%d %H:%M:%S")
+
+    shared_processors = [
+        timestamper,
+        structlog.stdlib.add_log_level,
+        structlog.contextvars.merge_contextvars,
+        structlog.processors.CallsiteParameterAdder(
+            {
+                structlog.processors.CallsiteParameter.FILENAME,
+                structlog.processors.CallsiteParameter.FUNC_NAME,
+                structlog.processors.CallsiteParameter.THREAD,
+                structlog.processors.CallsiteParameter.PROCESS,
+            }
+        ),
+        structlog.stdlib.ExtraAdder(),
+    ]
+
+    structlog.configure(
+        processors=shared_processors
+        + [structlog.stdlib.ProcessorFormatter.wrap_for_formatter],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        # call log with await syntax in thread pool executor
+        wrapper_class=structlog.stdlib.AsyncBoundLogger,
+        cache_logger_on_first_use=True,
+    )
+
+    logs_render = (
+        structlog.processors.JSONRenderer()
+        if enable_json_logs
+        else structlog.dev.ConsoleRenderer(colors=True)
+    )
+
+    log_dir = "../logs/"
+    log_file = log_dir + "app.log"
+    makedirs(log_dir, exist_ok=True)
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "default": {
+                "()": "uvicorn.logging.DefaultFormatter",
+                "fmt": "%(levelprefix)s %(message)s",
+                "use_colors": None,
+            },
+            "access": {
+                "()": "uvicorn.logging.AccessFormatter",
+                "fmt": "%(levelprefix)s %(client_addr)s - '%(request_line)s' %(status_code)s",
+            },
+            "json": {
+                "()": "structlog.stdlib.ProcessorFormatter",
+                "foreign_pre_chain": shared_processors,
+                "processors": [
+                    structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                    logs_render,
+                ],
+            },
+        },
+        "handlers": {
+            "console": {
+                "level": "DEBUG",
+                "class": "logging.StreamHandler",
+                "formatter": "json",
+            },
+            "file": {
+                "level": "DEBUG",
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "json",
+                "filename": log_file,
+                "maxBytes": 100_1000,
+                "backupCount": 3,
+                "encoding": "utf-8",
+            },
+            "default": {
+                "formatter": "json",
+                "class": "logging.StreamHandler",
+                "stream": "ext://sys.stdout",
+            },
+        },
+        "loggers": {
+            "": {
+                "handlers": ["console"],
+                "level": "INFO",
+            },
+            "uvicorn.error": {
+                "level": "INFO",
+            },
+            "uvicorn.access": {
+                "handlers": ["console", "file"],
+                "level": "INFO",
+                "propagate": False,
+            },
+        },
+        "root": {
+            "level": "INFO",
+            "formatter": "verbose",
+            "handlers": ["console", "file"],
+        },
+    }
+    logging.config.dictConfig(LOGGING)
+
+
+configure_logger(enable_json_logs=True)
+ugc_logger = structlog.get_logger()
